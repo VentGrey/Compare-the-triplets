0ba3d98747be3201c54047702c9b6717e4e646c4
VentGrey <omarpurataf@gmail.com>
2018-12-10 21:49:38 -0600

    Added problem description.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+//Compare the triplets.
+
+/* Alice and Bob each created one problem for @HackerRank. A reviewer rates the
+two challenges, the first being Alice, awarding points on a scale from 1 to 100
+for three categories: problem clarity, originality and difficulty.
+
+We define the rating for Alice's challenge to be the triplet 'a' being
+a = [a[0], a[1], a[2]]
+
+And the rating for Bob's challenge to be the triplet 'b' being
+b = [b[0], b[1], b[2]]
+
+Your task is to find their comparison points by comparing:
+
+a[0] with b[0]
+a[1] with b[1]
+a[2] with b[2]
+
+Conditions:
+if a[i] > b[i] -> Alice is awarded with one point.
+
+if a[i] < b[i] -> Bob is awarded with one point.
+
+if a[i] == b[i] -> Neither Alice nor Bob receive a point.
+
+*/
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
 }
